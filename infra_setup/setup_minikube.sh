e3a3e2d8358b6206a627c73f7ca741f694320f7d
Santhosh Kumar <43289678+santhoshkumarasamy@users.noreply.github.com>
2026-08-11 15:53:02 +0530

    argo-cd

--- infra_setup/setup_minikube.sh
+++ infra_setup/setup_minikube.sh
@@ -3,9 +3,10 @@
 # Install minikube
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-
+minikube version
 
 # Install kubectl
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
