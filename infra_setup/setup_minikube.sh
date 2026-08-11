212166da0b26f03e187f35d1a58b8be130d70bf2
Santhosh Kumar <43289678+santhoshkumarasamy@users.noreply.github.com>
2026-08-11 16:05:02 +0530

    argo-cd

--- infra_setup/setup_minikube.sh
+++ infra_setup/setup_minikube.sh
@@ -10,3 +10,7 @@ minikube version
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
+
+minikube start --nodes=2
+
+minikube addons enable ingress
