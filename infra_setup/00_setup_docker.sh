87e55aceadd540a3b161679904f8264988dcd56e
Santhosh Kumar <43289678+santhoshkumarasamy@users.noreply.github.com>
2026-08-20 20:21:28 +0530

    updates

--- infra_setup/00_setup_docker.sh
+++ infra_setup/00_setup_docker.sh
@@ -19,7 +19,7 @@ EOF
 
 sudo apt update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
 
--- infra_setup/04_argo-cd.sh
+++ infra_setup/04_argo-cd.sh
@@ -1,2 +1,5 @@
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install my-argo-cd argo/argo-cd --version 10.3.2
+
+
+kubectl -n default get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
