3d422f53a7a34e434a27ab677af5f316e4a23ec8
Santhosh Kumar <43289678+santhoshkumarasamy@users.noreply.github.com>
2026-08-21 07:05:08 +0530

    updates

--- infra_setup/04_argo-cd.sh
+++ infra_setup/04_argo-cd.sh
@@ -1,5 +1,5 @@
 helm repo add argo https://argoproj.github.io/argo-helm
-helm install my-argo-cd argo/argo-cd --version 10.3.2
+helm install my-argo-cd argo/argo-cd --version 10.3.2 -f helm_values.yaml
 
 
 kubectl -n default get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
